a6c354dce34ade4b550583d2082b1d4e872bdaeb
Dmitry Kulik <k.dmitri.sales@yandex.ru>
2023-09-08 16:47:27 +0300

    created detal card

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -1,6 +1,8 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
+
 import { BASE_URL } from "../../utils/constants";
+import { shuffle } from "../../utils/common";
 
 export const getProducts = createAsyncThunk(
     "products/getProducts",
@@ -20,12 +22,16 @@ const productsSlice = createSlice({
     initialState: {
         list: [],
         filtered: [],
-        // related: [],
+        related: [],
         isLoading: false,
     },
     reducers: {
         filterByPrice: (state, { payload }) => {
             state.filtered = state.list.filter(({ price }) => price < payload);
+        },
+        getRelatedProducts: (state, { payload }) => {
+            const list = state.list.filter(({ category: { id } }) => id === payload);
+            state.related = shuffle(list);
         },
     },
     extraReducers: (builder) => {
@@ -42,6 +48,6 @@ const productsSlice = createSlice({
     },
 });
 
-export const { filterByPrice } = productsSlice.actions;
+export const { filterByPrice, getRelatedProducts } = productsSlice.actions;
 
 export default productsSlice.reducer;
